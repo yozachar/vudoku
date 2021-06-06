afd8dd936a278c6f9a5efe5cddbf83792d35844e
Jovial Joe Jayarson <jovial7joe@hotmail.com>
2021-06-07 00:20:16 +0530

    🚧 tries streamlit_webrtc (untested)

--- src/main.py
+++ src/main.py
@@ -10,49 +10,38 @@ from extractor import de, np
 from solver import SudokuSolver
 
 import streamlit as st
-from webcam import webcam
+from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
 
-image = webcam()
 isSolved = False
 bw_sdk = p_string = s_board = None
 
 st.title("Vudoku")
 
+col1, col2 = st.beta_columns(spec=2)
 
-if image is None:
-    st.write("Waiting for capture...")
-else:
-    st.write("Got an Image")
-    image = np.asarray(image)
-    bw_sdk = sr.relay(image=image)  # returns a black and white sudoku grid
-    p_string = de.miner(image=bw_sdk)  # returns a 81 bit string
-    ss = SudokuSolver(string=p_string)
-    isSolved, s_board = ss.solveRecursively()  # returns puzzle status
+class Receiver(VideoProcessorBase):
 
-print(s_board)
+    def transform(self, frame):
+        image = frame.to_ndarray(format='bgr24')
+        
+        yield image # need to work on generators
 
+        bw_sdk = sr.relay(image=image)  # returns a black and white sudoku grid
+        p_string = de.miner(image=bw_sdk)  # returns a 81 bit string
+        ss = SudokuSolver(string=p_string)
+        isSolved, s_board = ss.solveRecursively()  # returns puzzle status and 
+        # print(s_board) # convert this to an image
+        # output = scv.sr.relay(image=image)
+        if isSolved:
+            return s_board
 
-# from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
-
-# col1, col2 = st.beta_columns(spec=2)
-# dummy checker box
-
-# class Receiver(VideoProcessorBase):
-
-#     def transform(self, frame):
-#         global output
-#         image = frame.to_ndarray(format='bgr24')
-#         # scv.img = img
-#         # img = cv.cvtColor(cv.Canny(img, 100, 200), cv.COLOR_GRAY2BGR)
-#         # output = scv.sr.relay(image=image)
-#         return image
-#         # return scv.result
+        # return scv.result
 
 
-# with col1:
-#     st.title('Input')
-#     webrtc_streamer(key='Input')
+with col1:
+    st.header(body='Input')
+    webrtc_streamer(key='Output', video_transformer_factory=Receiver)
 
-# with col2:
-#     st.title('Output')
-#     webrtc_streamer(key='Output', video_transformer_factory=Receiver)
+with col2:
+    st.header(body='Output')
+    st.image(image='') # final image
