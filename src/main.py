b1ebae30af4a9c2354cfdf62dda1a3a6dcab9b97
Jovial Joe Jayarson <jovial7joe@hotmail.com>
2021-06-08 20:31:25 +0530

    🚧 brings UI back to main

--- src/main.py
+++ src/main.py
@@ -5,30 +5,97 @@ The project path is laid out as follows:
 Video Input --> Sudoku Grabber -->  Solver --> Visualizer
 '''
 
-from artist import cap_img, Layout, st
-from solver import SudokuSolver
-from extractor import DigitExtractor, np
-from scavenger import SudokuRecognizer, cv
+from solver import slr
+from extractor import dex, np
+from scavenger import srg, cv
+
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
+
+
+# capture < edges < contours < transform < solve
+# capture = edges = contours = transform = solve = False
+capture_img = dilated_img = contour_img = flipped_img = None
 
 isSolved = False
 bw_sdk = p_string = s_board = None
-image = cv.cvtColor(src=cv.imread(filename='src/assets/images/sample/result.jpg'), code=cv.COLOR_BGR2RGB)
+sampled_img = cv.cvtColor(src=cv.imread(
+    filename='src/assets/images/sample/sudoku10.jpg'), code=cv.COLOR_BGR2RGB)
 
-lyt = Layout()
-dex = DigitExtractor()
-srg = SudokuRecognizer()
+bw_sdk = srg.relay(image=sampled_img)  # returns a black and white sudoku grid
+p_string = dex.miner(image=bw_sdk)  # returns a 81 bit string
+isSolved, s_board = slr.gateway(string=p_string)
 
-if lyt.capture:
-    image = cap_img if cap_img else image
+st.markdown("<h1 style='text-align: center;'>Vudoku - Visual Sudoku Solver</h1>",
+            unsafe_allow_html=True)
 
-if lyt.edges or lyt.contours or lyt.transform:
-    bw_sdk = srg.relay(image=image)  # returns a black and white sudoku grid
 
-if lyt.solve:
-    p_string = dex.miner(image=bw_sdk)  # returns a 81 bit string
-    ss = SudokuSolver(string=p_string)
-    isSolved, s_board = ss.solveRecursively()  # returns puzzle status and 
-# print(s_board) # convert this to an image
+class Receiver(VideoProcessorBase):
+    # Overridden class
 
-lyt.sample = image
-lyt.topCols()
+    def transform(self, frame):
+        # Overridden function
+        global capture_img
+        capture_img = frame.to_ndarray(format='bgr24')
+        return capture_img
+
+
+col1, col2 = st.beta_columns(spec=2)
+with col1:
+    # 1. Input
+    st.header(body='Input')
+    webrtc_streamer(key='Output', video_transformer_factory=Receiver)
+
+with col2:
+    # 5. Output
+    st.header(body='Output')
+    solve = st.button(label='Solve Sudoku', help='Solve Sudoku')
+    if solve:
+        # capture = edges = contours = transform = True
+        st.image(image=sampled_img)
+
+
+col1, col2 = st.beta_columns(spec=2)
+with col1:
+    # 2. Capture image
+    st.header('Captured Image')
+    capture = st.button(label='Capture Image',
+                        help='Capture Input Image')
+    if capture:
+        sampled_img = capture_img if capture_img else sampled_img
+        st.image(image=sampled_img)
+
+with col2:
+    # 3. Detect Edges
+    st.header('Detect Edges')
+    edges = st.button(label='Detect Edges',
+                      help='Uses canny edge detector')
+    if edges:
+        # capture = True
+        dilated_img = cv.cvtColor(src=cv.imread(
+            filename='src/assets/images/out/dilated.jpg'), code=cv.COLOR_BGR2RGB)
+        st.image(image=dilated_img)
+
+
+col1, col2 = st.beta_columns(spec=2)
+with col1:
+    # 3.Mark contour points
+    st.header('Mark Contours')
+    contours = st.button(label='Mark Contour Points',
+                         help='Mark Contour Points')
+    if contours:
+        # capture = edges = True
+        contour_img = cv.cvtColor(src=cv.imread(
+            filename='src/assets/images/out/contours.jpg'), code=cv.COLOR_BGR2RGB)
+        st.image(image=contour_img)
+
+with col2:
+    # 4. Perspective Transform
+    st.header('Perspective Transform')
+    transform = st.button(
+        label='Perspective Transform', help='Perspective Transform')
+    if transform:
+        # capture = edges = contours = True
+        flipped_img = cv.cvtColor(src=cv.imread(
+            filename='src/assets/images/out/flipped.jpg'), code=cv.COLOR_BGR2RGB)
+        st.image(image=flipped_img)
